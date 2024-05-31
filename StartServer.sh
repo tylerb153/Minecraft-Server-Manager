cfd902ceb7d3088503056856d9f62fc0e5572851
Tyler Bischoff <tbischoff2@ymail.com>
2024-05-31 16:45:44 -0400

    added condition if running already

--- StartServer.sh
+++ StartServer.sh
@@ -2,5 +2,7 @@
 if ! pgrep -f "java -Xmx12G -jar fabric-server-launch.jar nogui" >/dev/null; then
     echo "Starting server"
     # java -Xmx12G -jar fabric-server-launch.jar nogui
+else 
+    echo "Server already running"
 fi
 
