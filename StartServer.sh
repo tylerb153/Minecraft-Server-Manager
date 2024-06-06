f1ad8164ab5c90debb9e355849fd9b4ee30c4064
Tyler Bischoff <tbischoff2@ymail.com>
2024-06-06 11:17:14 -0400

    Changed to echo true or false and nothing else

--- StartServer.sh
+++ StartServer.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if ! pgrep -f "java -Xmx12G -jar fabric-server-launch.jar nogui" >/dev/null; then
-    echo "Starting server"
-    (cd ~/"Desktop/Modded Minecraft Server" && exec java -Xmx12G -jar fabric-server-launch.jar nogui) &
+    echo true
+    # echo "Starting server"
+    # (cd ~/"Desktop/Modded Minecraft Server" && exec java -Xmx12G -jar fabric-server-launch.jar nogui) &
 else 
-    echo "Server already running"
+    # echo "Server already running"
+    echo false
 fi
 
